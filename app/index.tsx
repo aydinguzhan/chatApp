f405a3f89bc5447dfdf0cfd275204d3f6df1316d
Oguzhan <aydinoguzhan2335@gmail.com>
2024-12-08 19:23:48 +0300

    Commit message

--- app/index.tsx
+++ app/index.tsx
@@ -26,6 +26,7 @@ export default function Index() {
       <StatusBar animated={true} barStyle={"default"} backgroundColor={COLOR_PALETTE.FIRST_PURPLE} />
 
       <Navbar page={page} goBack={() => setPage(pageName[0])} isConnect={null} />
+
       {page === pageName[0] && <Main setSelectUser={setSelectUser} />}
       {page === pageName[1] && <Chat adminCode={detail[0].participants[0]} chatDetail={detail} goBack={() => setPage(pageName[0])} />}
 
